6a2e1f473f509eb049cf4df824ccfee196f7340b
awes0m <somsubhra.pandit@gmail.com>
2022-12-01 16:04:18 +0530

    added Node Js serer, signup setup

---
+++ ama_clone/server/index.js
@@ -0,0 +1,35 @@
+//IMPORTS
+const express = require("express");
+const mongoose = require("mongoose");
+
+// IMPORT FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+// INIT
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://Somongoadmin:somongoadmin123@clustered.qag1arw.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+
+//crud
+// app.get('/hello-world',(req,res)=>{});
+
+
+//Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful to DB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
